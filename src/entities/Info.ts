c1241b335a60a757809df499b2c55420341f1713
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-01-25 19:41:55 -0300

    entities was full write and the propertys of tsconfig.json was changed to false this propertys strict and strictPropertyInitialization

--- src/entities/Info.ts
+++ src/entities/Info.ts
@@ -1,6 +1,30 @@
-import {Entity} from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+@Entity("info")
+class Info {
+  @PrimaryColumn()
+  readonly id: string;
 
-@Entity()
-export class Info {
+  @Column()
+  text: string;
 
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
+
+export { Info };
--- src/entities/Task.ts
+++ src/entities/Task.ts
@@ -1,6 +1,33 @@
-import {Entity} from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
 
-@Entity()
-export class Task {
+@Entity("task")
+class Task {
+  @PrimaryColumn()
+  readonly id: string;
 
+  @Column()
+  title: string;
+
+  @Column()
+  check: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
+export { Task };
