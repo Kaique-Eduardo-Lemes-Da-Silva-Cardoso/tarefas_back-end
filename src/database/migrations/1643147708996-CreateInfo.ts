e8d5597bfde261a6401c33877ba37ee243a14f8a
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-01-25 19:19:22 -0300

    the migrations and the tables was created

--- src/database/migrations/1643147708996-CreateInfo.ts
+++ src/database/migrations/1643147708996-CreateInfo.ts
@@ -1,11 +1,36 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateInfo1643147708996 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "info",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "text",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ]
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("info");
+  }
 }
