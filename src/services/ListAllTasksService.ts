d8ec990f21da255e7b03ec48ccdb7a945fb34028
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-02-13 16:52:43 -0300

    filter to get all

--- src/services/ListAllTasksService.ts
+++ src/services/ListAllTasksService.ts
@@ -5,7 +5,7 @@ class ListAllTasksService{
     async execute(){
         const taskRepositories = getCustomRepository(TaskRepositories);
         const allTasks  = await taskRepositories.find();
-        const tasks = allTasks.map(data=>({"id":data.id,"title":data.title,"completed":data.check}))
+        const tasks = allTasks.map(data=>({"id":data.id,"title":data.title,"completed":data.check, "infoId":data.infoId}))
         return  tasks;
     }
 }
